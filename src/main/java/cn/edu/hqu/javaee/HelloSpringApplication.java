259c631a78b029d22dd5ee11e9b12d5e94e336bc
xiaojiangtongxue <1457232341@qq.com>
2019-09-19 16:42:08 +0800

    first

---
+++ src/main/java/cn/edu/hqu/javaee/HelloSpringApplication.java
@@ -0,0 +1,14 @@
+package cn.edu.hqu.javaee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringApplication {
+
+	public static void main(String[] args) {
+		System.out.print("ÄãºÃ Spring");
+		SpringApplication.run(HelloSpringApplication.class, args);
+	}
+
+}
